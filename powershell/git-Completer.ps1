3f5dd897574dba815326094bd6bc82fd39009f75
Enoch Pang <enoch@enochpang.com>
2023-06-09 14:56:04 -0400

    Refactor git-Completer

--- powershell/git-Completer.ps1
+++ powershell/git-Completer.ps1
@@ -1,18 +1,17 @@
-$scriptblock = {
+
+Register-ArgumentCompleter -Native -CommandName git -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
 
     $sections = $commandAst.CommandElements | ForEach-Object { "$_" }
 
-    if ($sections[1] -in "checkout", "co") {
-        if ($sections[3] -eq "--") {
-
+    switch ($sections[1]) {
+        { $_ -in "checkout", "co" -and $sections[3] -eq "--" } {
             $fileList = @(git ls-tree $sections[2] -r -t --name-only)
-            $fileList -like "$wordToComplete*" 
+            return $fileList -like "$wordToComplete*"
         }
-
-        $branchList = @(git branch --format='%(refname:short)')
-        $branchList -like "$wordToComplete*" 
+        { $_ -in "checkout", "co" } {
+            $branchList = @(git branch -a --format='%(refname:short)')
+            return $branchList -like "$wordToComplete*"
+        }
     }
 }
-
-Register-ArgumentCompleter -Native -CommandName git -ScriptBlock $scriptblock
